2ff34d97052f336b26a556af4959ed3773c7c2f6
Jasmin Kling <jasmin.kling@exxcellent.de>
2021-12-23 11:58:57 +0100

    add script and updated README, TODO: add images

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# daily-workout-reminder
-💪 reminder and tracker of your daily workouts
+# :muscle: daily-workout-reminder-widget
+
+This widget reminds you of your daily workouts and tracks them.
+
+## Widget Design
+<img src="./img/widget.png" align="left" width="200" height="200">
+The widget is split into three parts. The top part displays the status of the workouts that were tracked over the past 5 days. The number at the beginning of each line indicates the workout count. Next, the status for the previous workout day is shown. The status is color or image coded. A blue :heavy_check_mark: indicates that the workout was completed. A red :x: indicates that the workout was skipped. Further, workouts such as walking (:walking:), running (:runner:), biking (:bike:) or playing tennis (:tennis:) are image coded.</br>
+The middle part displays todays date.</br>
+And the bottom part shows the status of todays workouts. These are color coded:
+* Gray indicates that the workout was not started yet.
+* Dark gray indicates that the current workout state is pending.
+* Green indicates that the workout was started.
+* Blue indicates that the workout was completed.
+* Red indicates that the workout was skipped.
+
+</br></br>
+
+## Functionality
+This widget reminds you of your daily workouts via notifications at the beginning and end of each workout and tracks if you completed or skipped the workout and what kind of workout you performed.</br>
+At the beginning and end of each workout a notification is send:</br>
+<img src="./img/notification_start.jpg" width="276" height="55">&nbsp;&nbsp;
+<img src="./img/notification_end.jpg" width="276" height="55"></br>
+You can interact with the notifiction to provide information about your current workout status.</br>
+You can ignore the notification. This changes the workout status to pending. If an end notifiction is send, but the start notifiction is still unanswered, the start notification is removed.</br>
+If you dissmiss a start notification, the workout status changes to not-started. Dissmissing an end notification indicates that you skipped the workout.</br>
+A long press on the notification, provides the following actions:</br>
+<img src="./img/notification_start_long_press.jpg">&nbsp;&nbsp;
+<img src="./img/notification_end_long_press.jpg"></br>
+A normal press on the notification opens the following UI. You are asked, if your started/finished your workout. In case you provide no as an answer, you are further asked, if you did a different workout. Here you can select a specific workout choice.
+<img src="./img/notification_start_ui1.PNG">&nbsp;&nbsp;
+<img src="./img/notification_start_ui2.PNG"></br>
+In case you provide a specific workout in response to a start notification, the end notification will not be send.
+
+## Requirements
+* Apple Device with iOS 14.
+* Scriptable latest (https://scriptable.app/).
+
+## Setup
+1. Copy the source code for ```daily-workout-reminder.js``` ("raw").
+2. Open Scriptable.
+3. Select "+" and insert the copy of the script.
+4. Choose the title of the script (e.g. Daily Workout Reminder).
+5. Save with "Done".
+6. Go back to the iOS Homescreen and get into the "wiggle mode".
+7. Press the "+" symbol and look for "Scriptable".
+8. Choose widget size (small) and "Add widget".
+9. Go into the settings of the widget to edit it.
+   * Choose script of step #4.
+
+## Further Config Parameter
+The top part of the script sets default config parameter. Those are:
+* locale: Specifies the locale for formatting the current date
+* shortcutNameHomeScreen: Specifies the name of the shortcut that redirects back to the homescreen. The shortcut must be created in addition, or it can be left empty/undefined.
+* startTimes: Specifies the start times of each workout. The length of the underlying array determines the amount of workouts per day
+* duration: Specifies the duration for each workout
+
+**Enjoy the widget!**
---
+++ daily-workout-reminder.js
@@ -0,0 +1,333 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: dumbbell;
+
+// start of config ==========================
+
+// sets the locale of the date
+let locale = "de_DE";
+
+// sets the name of the shortcut that redirects back to the homescreen (can be undefined)
+let shortcutNameHomeScreen = "hs";
+
+// how many and when do you want to start your workouts on each week day?
+let startTimes = ["06:30", "14:00", "21:30"];
+
+// workout time in min
+let duration = 30;
+
+// end of config ===========================
+
+const WorkoutStates = {
+  NOT_STARTED: 0,
+  PENDING: 1,
+  SKIPPED: 2,
+  STARTED: 3,
+  DONE: 4,
+  TENNIS: 5,
+  RUNNING: 6,
+  BIKE: 7,
+  WALKING: 8,
+  
+  properties: {
+    0: {name: "Not started", value: 0, color: Color.gray()},
+    1: {name: "Pending", value: 1, color: Color.darkGray()},
+    2: {name: "Skipped", value: 2, color: Color.red()},
+	3: {name: "Started", value: 3, color: Color.green()},
+	4: {name: "Done", value: 4, color: Color.blue()},
+	5: {name: "Tennis", value: 5, color: Color.blue(), text: "🎾"},
+	6: {name: "Running", value: 6, color: Color.blue(), text: "🏃"},
+	7: {name: "Bike", value: 7, color: Color.blue(), text: "🚴"},
+	8: {name: "Walking", value: 8, color: Color.blue(), text: "🚶"},
+  }
+};
+
+// initialize date formatter for time and date
+let dft = new DateFormatter();
+dft.useShortTimeStyle();
+let df = new DateFormatter();
+df.useMediumDateStyle();
+df.locale = locale;
+
+// load and initialize data
+let fm = FileManager.iCloud();
+let file = fm.joinPath(fm.documentsDirectory(), "dailyworkout.json");
+let data = fm.fileExists(file) ? JSON.parse(fm.readString(file)) : JSON.parse("[]");
+let todaysDate = df.date(df.string(new Date()));
+let dataToday = (data.length > 0 && sameDay(todaysDate, df.date(data[0].date))) ? data[0] : JSON.parse("{}");
+
+if (args.queryParameters.workoutCount && args.queryParameters.state) {
+	// updates data based on query parameters
+	dataToday.state[parseInt(args.queryParameters.workoutCount) - 1] = parseInt(args.queryParameters.state);
+	updateAndSave(file, data, dataToday);
+	backToHomeScreen(shortcutNameHomeScreen);
+	Script.complete();
+} else if (config.runsInNotification) {
+	// nothing to show (but notification options are shown)
+	Script.complete();
+} else if (args.notification) {
+	// run from notification
+	let notif = args.notification;
+	let isStart = notif.userInfo["isStart"];
+	workoutCount = notif.userInfo["workoutCount"];
+	
+	let ui = new UITable();
+	ui.showSeparators = true;
+	let rowH, rowQ, rowA, cell;
+
+	rowH = new UITableRow();
+	rowH.isHeader = true;
+	rowH.height = 80;
+	rowH.addText("WORKOUT").centerAligned();
+	ui.addRow(rowH);
+
+	rowQ = new UITableRow();
+	rowQ.height = 60;
+	if (isStart) {
+		rowQ.addText("Workout started?").centerAligned();
+	} else {
+		rowQ.addText("Workout finished?").centerAligned();
+	}
+	ui.addRow(rowQ);
+	
+	rowA = new UITableRow();
+	rowA.height = 40;
+	ui.addRow(rowA);
+
+	cell = rowA.addButton("Yes");
+	cell.dismissOnTap = true;
+	cell.centerAligned();
+	cell.onTap = () => {
+		dataToday.state[workoutCount - 1] = (isStart ? WorkoutStates.STARTED : WorkoutStates.DONE);
+		updateAndSave(file, data, dataToday);
+	};
+	cell = rowA.addButton("No");
+	cell.dismissOnTap = false;
+	cell.centerAligned();
+	cell.onTap = () => {
+		ui.removeRow(rowQ);
+		ui.removeRow(rowA);
+		rowQ = new UITableRow();
+		rowQ.height = 60;
+		rowQ.addText("Did you do a different workout?").centerAligned();
+		ui.addRow(rowQ);
+		rowA = new UITableRow();
+		rowA.height = 40;
+		ui.addRow(rowA);
+		let buttonNo = rowA.addButton("No");
+		buttonNo.centerAligned();
+		buttonNo.dismissOnTap = true;
+		buttonNo.onTap = () => {
+			dataToday.state[workoutCount - 1] = (isStart ? WorkoutStates.NOT_STARTED : WorkoutStates.SKIPPED);
+			updateAndSave(file, data, dataToday);
+		};
+		for (let state in WorkoutStates) {
+			state = WorkoutStates[state];
+			if (state > WorkoutStates.DONE) {
+				let row = new UITableRow();
+				row.height = 40;
+				let button = row.addButton(WorkoutStates.properties[state].name);
+				button.centerAligned();
+				button.dismissOnTap = true;
+				button.onTap = () => {
+					dataToday.state[workoutCount - 1] = state;
+					updateAndSave(file, data, dataToday);
+				};
+				ui.addRow(row);
+			}
+		}
+		ui.reload();
+	};
+
+	await ui.present();
+	backToHomeScreen(shortcutNameHomeScreen);
+	Script.complete();
+} else if (config.runsInApp || config.runsInWidget || config.runsFromHomeScreen){
+	// build widget
+	let widget = new ListWidget();
+	widget.backgroundColor = Color.lightGray();
+
+	// add workout history of past 5 days to widget
+	let pastWorkouts = getDataForPastNDays(data, todaysDate, 5, df);
+	let contentStack = widget.addStack();
+	contentStack.layoutVertically();
+	for (workoutCount = 1; workoutCount <= startTimes.length; workoutCount++) {
+		let stack = contentStack.addStack();
+		stack.size = new Size(contentStack.size.width, 20);
+		stack.addImage(SFSymbol.named(`${workoutCount}.circle.fill`).image);
+		pastWorkouts.forEach((entry) => {
+			let state = entry.state[workoutCount - 1];
+			if (state <= WorkoutStates.DONE) {
+				stack.addImage(SFSymbol.named(state >= WorkoutStates.STARTED ? "checkmark" : "xmark").image);		
+			} else {
+				stack.addText(WorkoutStates.properties[state].text);
+			}
+		});
+	}
+    contentStack.addSpacer(10);
+	  
+	if (isWeekDay(todaysDate)) {
+		let dataChanged = false;
+		let workoutCount = 0;
+		// check if data for today exists and use it
+		if (JSON.stringify(dataToday) === "{}") {
+			// remove notifications from another day
+			let pendingNotif = (await Notification.allPending()).filter(notif => notif.threadIdentifier === Script.name()).map(notif => notif.identifier);
+			await Notification.removePending(pendingNotif);
+			let deliveredNotif = (await Notification.allDelivered()).filter(notif => notif.threadIdentifier === Script.name()).map(notif => notif.identifier);
+			await Notification.removeDelivered(deliveredNotif);
+			// initialize data for today
+			console.log("add new data for today");
+			dataChanged = true;
+			dataToday.date = df.string(todaysDate);
+			dataToday.state = new Array(startTimes.length).fill(WorkoutStates.NOT_STARTED);
+			data.unshift(dataToday);
+			// schedule daily notifications
+			for (workoutCount = 1; workoutCount <= startTimes.length; workoutCount++) {		
+				// workout start: reminder that can just be dismissed - is dismissed with end
+				await createNotification(getDateTime(todaysDate, dft, startTimes[workoutCount - 1]), `Start your daily workout #${workoutCount} 💪`, workoutCount, true);
+				// workout end: reminder that asks if workout was done
+				let endWorkout = getDateTime(todaysDate, dft, startTimes[workoutCount - 1]);
+				endWorkout.setMinutes(endWorkout.getMinutes() + duration);
+				await createNotification(endWorkout, `💪 Finish your workout #${workoutCount}`, workoutCount, false);
+				
+			}
+		}
+		workoutCount = determineCurrentWorkoutCount(todaysDate, startTimes, dft);
+		// update notifs
+		pendingNotif = (await Notification.allPending()).filter(notif => notif.threadIdentifier === Script.name());
+		deliveredNotif = (await Notification.allDelivered()).filter(notif => notif.threadIdentifier === Script.name());
+		if (dataToday.state && dataToday.state[workoutCount - 1] >= WorkoutStates.DONE) {
+			// remove remaining notif for workout
+			let pendingNotifsToRemove = pendingNotif.filter(notif => notif.userInfo["workoutCount"] === workoutCount).map(notif => notif.identifier);
+			await Notification.removePending(pendingNotifsToRemove);
+			let deliveredNotifsToRemove = deliveredNotif.filter(notif => notif.userInfo["workoutCount"] === workoutCount).map(notif => notif.identifier);
+			await Notification.removeDelivered(deliveredNotifsToRemove);
+		} else if (deliveredNotif.some(notif => notif.userInfo["isStart"] === false && notif.userInfo["workoutCount"] === workoutCount) && deliveredNotif.some(notif => notif.userInfo["isStart"] === true)) {
+			// remove delivered start notif if end is delivered
+			let deliveredNotifsToRemove = deliveredNotif.filter(notif => notif.userInfo["isStart"] === true).map(notif => notif.identifier);
+			await Notification.removeDelivered(deliveredNotifsToRemove);
+		}
+		// change state depending on current state and dismissed/delivered/pending notifs
+		if (dataToday.state[workoutCount - 1] === WorkoutStates.NOT_STARTED && deliveredNotif.some(notif => notif.userInfo["workoutCount"] === workoutCount)) {
+			// change state to pending if unanswered start notif
+			dataToday.state[workoutCount - 1] = WorkoutStates.PENDING;
+			dataChanged = true;
+		} else if (dataToday.state[workoutCount - 1] === WorkoutStates.PENDING && deliveredNotif.every(notif => notif.userInfo["workoutCount"] !== workoutCount) && pendingNotif.some(notif => notif.userInfo["workoutCount"] === workoutCount)) {
+			// change state to not started if dismissed start notif
+			dataToday.state[workoutCount - 1] = WorkoutStates.NOT_STARTED;
+			dataChanged = true;
+		} else if (dataToday.state[workoutCount - 1] < WorkoutStates.DONE && deliveredNotif.every(notif => notif.userInfo["workoutCount"] !== workoutCount) && pendingNotif.every(notif => notif.userInfo["workoutCount"] !== workoutCount)) {
+			// change state to skipped if not done and no more notifs for workout count (dismissed start and end notifs)
+			dataChanged = dataToday.state[workoutCount - 1] !== WorkoutStates.SKIPPED;
+			dataToday.state[workoutCount - 1] = WorkoutStates.SKIPPED;
+		}
+		if (dataChanged) {
+			console.log(`dataToday: ${JSON.stringify(dataToday)}`);
+			updateAndSave(file, data, dataToday);
+		}
+    } else {
+		dataToday.date = df.string(todaysDate);
+		dataToday.state = new Array(startTimes.length);
+    }
+	
+	// add todays date to widget
+    contentStack.addText(dataToday.date);
+	
+	// add state of each workout to widget
+	let stackToday = contentStack.addStack();
+	dataToday.state.forEach((stateEntry, stateIndex) => {
+		let todaysWorkout = stackToday.addImage(SFSymbol.named(`${stateIndex+1}.circle`).image);	
+		todaysWorkout.imageSize = new Size(contentStack.width / startTimes.length, contentStack.height / startTimes.length);
+		todaysWorkout.tintColor = stateEntry == null ? Color.lightGray() : WorkoutStates.properties[stateEntry].color;
+		stackToday.addSpacer(10);
+	});
+	// run widget
+	if (!config.runsInWidget) {
+	  await widget.presentSmall();
+	}
+	Script.setWidget(widget);
+	Script.complete();
+}
+
+// helper func
+
+// determines the current workout count
+function determineCurrentWorkoutCount(todaysDate, startTimes, dft) {
+	let date = new Date();
+	for (let startIdx = 0; startIdx < startTimes.length; startIdx++) {
+		let workoutStartDateTime = getDateTime(todaysDate, dft, startTimes[startIdx])
+		if (date.getTime() < workoutStartDateTime.getTime()) {
+			return Math.max(1, startIdx);
+		}
+	}
+	return startTimes.length;
+}
+
+// update and save data
+function updateAndSave(file, data, dataToday) {
+	console.log(`saving data with ${data.length} entries`);
+	data[0] = dataToday;
+	fm.writeString(file, JSON.stringify(data));
+}
+
+// gets data for past n days
+function getDataForPastNDays(data, todaysDate, n, df) {
+	if (data.length > 0 && sameDay(todaysDate, df.date(data[0].date))) {
+		return data.slice(1, n+1);
+	}
+	return data.slice(0, n);
+}
+
+// determines if two dates are on the same day
+function sameDay(first, second) {
+    return ((first.getFullYear() === second.getFullYear()) &&
+			(first.getMonth() === second.getMonth()) &&
+			(first.getDate() === second.getDate()));
+}
+
+// determines if given date is a weekday
+function isWeekDay(d) {
+	return ((d.getDay() > 0) // day 0 is sunday
+		&& (d.getDay() < 6)); // day 6 is saturday
+}
+
+// gets today with hours and minutes set to the given time
+function getDateTime(todaysDate, dft, formattedTime) {
+	let d = todaysDate;
+	d.setHours(dft.date(formattedTime).getHours());
+	d.setMinutes(dft.date(formattedTime).getMinutes());
+	return d;
+}
+
+// creates a new notification
+async function createNotification(date, reason, workoutCount, isStart) {
+        console.log(`schedule notif on ${date.toLocaleString()} for ${reason}`);
+		let notif = new Notification();
+		notif.title = Script.name();
+		notif.body = reason.toString();
+		notif.openURL = URLScheme.forRunningScript();
+		notif.sound = "popup";
+		notif.threadIdentifier = Script.name();
+        notif.userInfo = {"workoutCount": workoutCount, "isStart": isStart};
+		let state = isStart ? WorkoutStates.STARTED : WorkoutStates.DONE;
+		let stateProp = WorkoutStates.properties[state];
+        notif.addAction(stateProp.name, `${URLScheme.forRunningScript()}?workoutCount=${workoutCount}&state=${stateProp.value}`, true);
+		for (state in WorkoutStates) {
+			state = WorkoutStates[state];
+			if (state > WorkoutStates.DONE) {
+				stateProp = WorkoutStates.properties[state];
+				notif.addAction(stateProp.name, `${URLScheme.forRunningScript()}?workoutCount=${workoutCount}&state=${stateProp.value}`, true);
+			}
+		}
+		notif.setTriggerDate(date);
+		await notif.schedule();
+	}
+
+// calls the shortcut with the name hs: shortcut to got back to the homescreen
+function backToHomeScreen(shortcutNameHomeScreen) {
+	if (shortcutNameHomeScreen) {
+		Safari.open(`shortcuts://run-shortcut?name=${shortcutNameHomeScreen}`);
+	}
+}
+
